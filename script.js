96b2335ac7e31be866cd677a989366b330913c6d
shamo0 <genuka10@gmail.com>
2020-04-16 12:52:59 -0700

    javascript input validation beginning

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-  var $this = $(this),
-      label = $this.prev('label');
+function validateLogin() {
+  var email = document.forms["login"]["email"].value;
+  var password = documtn.forms["login"]["password"].value;
 
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
-          label.removeClass('active highlight');
-        } else {
-          label.addClass('active highlight');
-        }
-    } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
-    } else if (e.type === 'focus') {
-      
-      if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
-      else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
-    }
+  if (email == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+    return (true);
+  }
+    alert("You have entered an invalid email address!");
+    return (false);
+  //check for email in users database below
+  
+  // Password requesiremtns here and check in database
+  // we could store the hashes of passwords in database 
+  // to be more ligit and compare the hashes as the login test
+} 
 
-});
+function validateSignup() {
+  var email = document.forms["signup"]["email"].value;
+  var password = documtn.forms["signup"]["password"].value;
+  var name = document.forms["signup"]["name"].value;
 
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
-  target = $(this).attr('href');
+  if (email == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+    return (true);
+  }
+    alert("You have entered an invalid email address!");
+    return (false);
+  //check for email in users database below
 
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
-});
+
+  //This only checks regex for:
+  //[7 to 15 characters which 
+  //contain only characters, numeric digits, underscore and 
+  //first character must be a letter]
+  var passwReq=  /^[A-Za-z]\w{7,14}$/;
+  if(password.match(passwReq)) { 
+    alert('Correct, try another...')
+    return true;
+  }
+  else { 
+    alert('Wrong...!')
+    return false;
+  }
+  //pasword check needes in database
+}
